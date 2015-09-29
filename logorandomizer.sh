a3646ae14e70209b2e9f29887fbd7421f3d6afa1
Edson Ticona <xcr33d@gmail.com>
2015-09-29 18:02:05 -0500

    added regex for matching bezier curves and a verbose option

--- logorandomizer.sh
+++ logorandomizer.sh
@@ -4,13 +4,15 @@
 num=1
 height=256
 width=256
+verbose=0
 convert_options=""
-while getopts "hf:n:r:" opt
+while getopts "hf:n:r:v" opt
 do
 	case $opt in
 		h)
 			echo "Form of use"
-			echo "./logoRandomizer -f filename -n <number_of_images> -r <height>x<width>"
+			echo "./logoRandomizer -f filename -n <number_of_images> -r <height>x<width> "
+			echo "-v for verbosing"
 			exit 0
 		;;
 		n)
@@ -27,6 +29,9 @@ do
 		f)
 			inputfile=$OPTARG
 			fname=`echo $OPTARG | cut -d. -f1`
+		;;
+		v)
+			verbose=1
 		;;
 	esac
 done
@@ -79,59 +84,90 @@ function_resize(){
 ######### Set of transformation functions ##########
 ####################################################
 blur(){		#ID 0 
-	echo "Blur"
 #sleep 1s
 	radius=`expr $RANDOM % 3`
 	sigma=`expr $RANDOM % 3`
 	#convert -blur "${radius}x${sigma}" $1 "${outfile}.blur.png"
 	convert_options="$convert_options -blur ${radius}x${sigma}"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== Blur params===="
+		echo "radius: $radius"
+		echo "sigma: $sigma"
+	fi
 }
 
 whitenoise(){	#ID 1
-	echo "Whitenoise"
 	at=0.05
 	#convert +noise Gaussian -attenuate $at $1 "${file}.noise.png"
 	#convert +noise Impulse -attenuate $at $1 "${outfile}.noise.png"
 	convert_options="$convert_options +noise Impulse -attenuate $at"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== Whitenoise params ===="
+		echo "attenuation: $at"
+	fi
 }
 
 grayscale(){	#ID 2
-	echo "Grayscale"
 	#convert -colorspace Gray $1 "${outfile}.grayscale.png"
 	convert_options="$convert_options -colorspace Gray"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== Grayscale params===="
+		echo "colorspace: Gray"
+	fi
 }
 
 colornegation(){	#ID 3
-	echo "ColorInvert"
 	#convert -negate $1 "${outfile}.invert.png"
 	convert_options="$convert_options -negate"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== ColorInvert params===="
+		echo "negate"
+	fi
 }
 
 modulatecolor(){	#ID 4
-	echo "Modulatecolor"
 	bright=100
 	saturation=80
 	hue=`expr $RANDOM % 100`
 #sleep 1s
 	#convert -modulate "$bright,$saturation,$hue" $1 "${outfile}.colormodulation.png"
 	convert_options="$convert_options -modulate $bright,$saturation,$hue"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== Modulatecolor params===="
+		echo "bright: $bright"
+		echo "saturation: $saturation"
+		echo "hue: $hue"
+	fi
 }
 
 rotation(){	#ID 5
-	echo "Rotation"
 	angle=`expr $RANDOM % 360`
 #sleep 1s
 	#convert -rotate $angle $1 "${outfile}.rotate.png"
 	convert_options="$convert_options -rotate $angle"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== Rotation params ===="
+		echo "angle: $angle"
+	fi
 }
 
 waving(){	#ID 6
-	echo "Waving"
 	amp=`expr $RANDOM % 10`
-#sleep 1s
 	lambda=`expr $RANDOM % 100`
 	#convert -wave "${amp}x${lambda}" $1 "${outfile}.waved.png"
 	convert_options="$convert_options -wave ${amp}x${lambda}"
+	if [ $verbose -eq 1 ]
+	then
+		echo "==== Waving params ===="
+		echo "amplitude: $amp"
+		echo "lambda: $lambda"
+	fi
 }
 
 editSVGTranslation(){	#ID 7
@@ -174,6 +210,13 @@ editSVGTranslation(){	#ID 7
 	#echo "=== valfx $valfx"
 	#echo "=== valfy $valfy"
 	sed -i "s/m $valx,$valy/m $valfx,$valfy/" $1
+	if [ "$verbose" -eq "1" ]
+	then
+		echo "==== editSVGTranslation params ===="
+		echo "line number (after matching): $ln"
+		echo "old x,y: $valx,$valy"
+		echo "new x,y: $valfx,$valfy"
+	fi
 }
 
 editSVGColor(){		#ID 8
@@ -190,17 +233,51 @@ editSVGColor(){		#ID 8
 	blue=`echo "obase=16; $blue" | bc`
 	nhexval="${red}${green}${blue}"
 	sed -i "s/$hexval/$nhexval/" $1
+	if [ "$verbose" -eq "1" ]
+	then
+		echo "==== editSVGColor params ===="
+		echo "line number (after matching): $ln"
+		echo "old color: $hexval"
+		echo "new color: $nhexval"
+	fi
 }
 
 editSVGBezier(){	#ID 9
 	lines=`grep -E "c [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]* [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]* [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]*" $1 | wc -l `
+	#echo 'grep -E "c [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]* [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]* [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]*" $1 | wc -l '
 	ln=`expr $RANDOM % $lines`
 	let "ln+=1"
 	cval=`grep -E "c [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]* [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]* [-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]*" $1 | sed -n ${ln},${ln}p `
-	multiplier="0.90 0.91 0.92 0.93 0.94 0.95 0.96 0.97 0.98 0.99 1.01 1.02 1.03 1.04 1.05 1.06 1.07 1.08 1.09 1.10"
+	#multiplier="0.90 0.91 0.92 0.93 0.94 0.95 0.96 0.97 0.98 0.99 1.01 1.02 1.03 1.04 1.05 1.06 1.07 1.08 1.09 1.10"
+	multiplier="1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0"
 	rnd=`expr $RANDOM % 20 + 1`
 	pp=`echo $multiplier | awk -F " " -v rnd=$rnd '{print $rnd}'`
-	#x1=echo $cval | awk -F " " '{print }'
+	if [ "$verbose" -eq "1" ]
+	then
+		echo "==== editSVGBezier params ===="
+		echo "line number (after matching): $ln"
+		echo "multiplying factor:  $pp"
+		echo "matching string: $cval"
+	fi
+	x1=`echo $cval | cut -d c -f 2 | cut -d " " -f 2 | cut -d , -f 1`
+	y1=`echo $cval | cut -d c -f 2 | cut -d " " -f 2 | cut -d , -f 2`
+	x2=`echo $cval | cut -d c -f 2 | cut -d " " -f 3 | cut -d , -f 1`
+	y2=`echo $cval | cut -d c -f 2 | cut -d " " -f 3 | cut -d , -f 2`
+	x=`echo $cval | cut -d c -f 2 | cut -d " " -f 4 | cut -d , -f 1`
+	y=`echo $cval | cut -d c -f 2 | cut -d " " -f 4 | cut -d , -f 2 | cut -d\" -f 1` #last cut is for some cases on which the line ends with quotation marks
+	#echo $x1
+	#echo $y1
+	#echo $x2
+	#echo $y2
+	#echo $x
+	#echo $y
+	nx1=`echo "$x1 * $pp" | bc`
+	ny1=`echo "$y1 * $pp" | bc`
+	nx2=`echo "$x2 * $pp" | bc`
+	ny2=`echo "$y2 * $pp" | bc`
+	nx=`echo "$x * $pp" | bc`
+	ny=`echo "$y * $pp" | bc`
+	sed -i "s/$x1,$y1 $x2,$y2 $x,$y/$nx1,$ny1 $nx2,$ny2 $nx,$ny/" $1 
 }
 
 ####################################################
@@ -245,8 +322,10 @@ then
 fi
 function_resize $svgfile $height $width
 #applyTransformations $svgfile 
-editSVGTranslation $svgfile
-editSVGColor $svgfile
+#editSVGTranslation $svgfile
+#editSVGColor $svgfile
+#editSVGBezier $svgfile
+#exit 0
 
 #supports up to 7! transformations
 for i in `seq 1 $num`
@@ -254,8 +333,9 @@ do
 	#FIXME: Check repetions to avoid them
 sleep 1s
 	convert_options=""
-	tr=`expr $RANDOM % 128`
-	echo $tr
+	#tr=`expr $RANDOM % 128`
+	tr=`expr $RANDOM % 1024`
+	#echo $tr
 	tblur=$(($tr&1))
 	twhno=$(($tr&2))
 	tgray=$(($tr&4))
@@ -263,6 +343,9 @@ sleep 1s
 	tmodu=$(($tr&16))
 	trota=$(($tr&32))
 	twave=$(($tr&64))
+	tSVGTranslate=$(($tr&128))
+	tSVGColor=$(($tr&256))
+	tSVGBezier=$(($tr&512))
 	if [ $tblur -eq 0 ]
 	then
 		blur
@@ -291,16 +374,23 @@ sleep 1s
 	then
 		waving
 	fi
+	if [ $tSVGTranslate -eq 0 ]
+	then
+		editSVGTranslation $svgfile
+	fi
+	if [ $tSVGColor -eq 0 ]
+	then
+		editSVGColor $svgfile
+	fi
+	if [ $tSVGBezier -eq 0 ]
+	then
+		editSVGBezier $svgfile
+	fi
 
-	echo "convert $convert_options $svgfile $outfile"
+	if [ $verbose -eq 1 ]
+	then
+		echo "convert $convert_options $svgfile $outfile"
+	fi
 	sc=`convert $convert_options $svgfile "$outfile.${i}.png"`
 done
-
-
-
-
-
-
-
-
 
